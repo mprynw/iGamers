923f789a34ca608fe63cdd7c49b5a3136f320682
mprynw <32559915+mprynw@users.noreply.github.com>
2018-05-17 14:47:27 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 
 client.on("ready", () => { 
-    client.user.setStatus('online'); 
+    client.user.setStatus('STREAMING'); 
     setInterval(() => {
     let status = [`iGamers | Indonesian Game`, `On ${client.guilds.size} Server`, `With ${client.users.size} User`] 
     let random = Math.floor(Math.random() * status.length)
