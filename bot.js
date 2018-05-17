3c2986ac18463bafe9931030fd0d14035ef4682b
mprynw <32559915+mprynw@users.noreply.github.com>
2018-05-17 12:22:37 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = required['discord.js'];
+const client = new Discord.Client();
+
+client.on('ready, {} => {
+    console.log{'I am ready!'};
+});
+
+client.on('message', message => {
+    if (message.content == 'iGamers') {
+      message.reply('Hai @mprw ini invite link untuk iGamers | Indonesian Game! \n Tolong digunakan dengan bijak ya :D \n pakai link ini yaa >> https://discord.gg/dR422Nu');
+       }
+});
+
+client.login(process.env.NDQzNjYzNTAxNTgwNDM1NDU3.Dd6XRQ.cG2hl5Qc6TkPbN5rFTYa3bHCubE)
