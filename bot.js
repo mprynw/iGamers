8de32b8d8c34c73749c76aecfd1d76f34607ae75
mprynw <32559915+mprynw@users.noreply.github.com>
2018-05-17 14:22:48 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,22 @@
 const Discord = require('discord.js');
-const client = new Discord.Bot();
+const client = new Discord.Client();
 
 
-bot.on("ready", () => { 
-    bot.user.setStatus('STREAMING'); 
+client.on("ready", () => { 
+    client.user.setStatus('STREAMING'); 
     setInterval(() => {
-    let status = [`Bantuan? | >help`, `InFamous Club`, `ForceStop Channel`, `On ${bot.guilds.size} Server`, `With ${bot.users.size} User`] 
+    let status = [`Bantuan? | >help`, `InFamous Club`, `ForceStop Channel`, `On ${client.guilds.size} Server`, `With ${client.users.size} User`] 
     let random = Math.floor(Math.random() * status.length)
-    bot.user.setGame(status[random], 'https://twitch.tv/forcestopgm'); 
+    client.user.setGame(status[random], 'https://twitch.tv/forcestopgm'); 
    }, 20000);
-    console.log(`${bot.user.username} Sedang ONLINE!`); 
+    console.log(`${client.user.username} Sedang ONLINE!`); 
 });
 
 
-bot.on('message', message => {
+client.on('message', message => {
     if (message.content === 'iGamers') {
       message.reply('Ini invite link untuk iGamers | Indonesian Game! \n Tolong digunakan dengan bijak ya :D \n pakai link ini yaa >> https://discord.gg/dR422Nu');
        }
 });
 
-bot.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN)
