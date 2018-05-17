d71378c665d9703e6d98016f7d627930d764edfa
mprynw <32559915+mprynw@users.noreply.github.com>
2018-05-17 13:00:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
-const Discord = required['discord.js'];
+const Discord = require('discord.js');
 const client = new Discord.Client();
+
 
 client.on('ready', {} => {
     console.log{'I am ready!'};
 });
 
 client.on('message', message => {
-    if (message.content == 'iGamers') {
-      message.reply('Hai @mprw ini invite link untuk iGamers | Indonesian Game! \n Tolong digunakan dengan bijak ya :D \n pakai link ini yaa >> https://discord.gg/dR422Nu');
+    if (message.content === 'iGamers') {
+      message.reply('Ini invite link untuk iGamers | Indonesian Game! \n Tolong digunakan dengan bijak ya :D \n pakai link ini yaa >> https://discord.gg/dR422Nu');
        }
 });
 
