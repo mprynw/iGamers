582bb2b89b25fcb6cac05be65f6212ca128cfdc0
mprynw <32559915+mprynw@users.noreply.github.com>
2018-05-20 06:12:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,13 +15,11 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 
